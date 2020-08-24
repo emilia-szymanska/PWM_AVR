c810a4b8c09f1e3aac1d6ca1b75ab6c7ec0b4611
emilia-szymanska <58346361+emilia-szymanska@users.noreply.github.com>
2020-08-24 07:29:14 +0200

    Moving PWM and UART to separate files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
-#include <avr/io.h>
+//commented includes as these libraries are included in PWM.cpp and uart.cpp
+//#include <avr/io.h>
+//#include <avr/interrupt.h>
 #include <util/delay.h>
-#include <avr/interrupt.h>
+#include "PWM.cpp"
+#include "uart.cpp"
 
 #define tbi(port, pin) (port) ^= _BV(pin)
 #define sbi(port, pin) (port) |= _BV(pin)
@@ -9,57 +12,6 @@
 #define MAX_servo 12.75    //max deviation in % (normally 2.55ms/20ms)
 #define MIN_servo 2.75     //min deviation in % (normally 0.55ms/20ms)
 #define MAX_degrees 180
-
-void uart_config()
-{
-  UBRR0H = 0x00;                          //USART baud rate register high
-  UBRR0L = 0x10;                          //USART baud rate register low
-  UCSR0C |= (1<<UCSZ01) | (1<< UCSZ00);   //8-bit character size
-  UCSR0B |= (1<< RXEN0) | (1<< TXEN0);    //Receiver & Transmitter enable
-}
-
-void USART_Transmit(unsigned char data)
-{
-  while (!(UCSR0A & (1<<UDRE0)));     //Wait for empty transmit buffer
-  UDR0 = data;                        //Put data into buffer, sends the data
-}
-
-unsigned char USART_Receive()
-{
-  while (!(UCSR0A & (1<<RXC0)));    //Wait for data to be received
-  return UDR0;                      //Get and return received data from buffer
-}
-
-void init_timer1(int frequency)
-{
-  TIMSK1 |= (1<<OCIE1B) | (1<<OCIE1A);  //Timer/Counter1, Output Compare A & B Match Interrupt Enable
-  TCCR1B |= (1<<WGM12);                 //Clear Timer on Compare Match (CTC) Mode
-  sei();
-
-  if(frequency < 4)                     //to small prescaler
-  {
-    TCCR1B |= (1<<CS12);                //prescaler = 256
-    OCR1A = 16000000/(256 * frequency) - 1;
-  }
-  else 
-  {
-    if(frequency > 100000)              //too big prescaler
-    {
-      TCCR1B |= (1<<CS11);              //prescaler = 8
-      OCR1A = 16000000/(8 * frequency) - 1;
-    }
-    else
-    {
-      TCCR1B |= (1<<CS11) | (1<<CS10);  //prescaler = 64
-      OCR1A = 16000000/(64 * frequency) - 1;
-    }
-  }
-}
-
-void set_duty_cycle(double duty_cycle)        //in % 
-{
-  OCR1B = ((OCR1A + 1) * duty_cycle)/100 - 1; //setting the duty cycle based on % value
-}
 
 void set_servo_position(int degrees)
 {
